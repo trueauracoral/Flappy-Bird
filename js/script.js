df5b3568e51ad59cc37e99f0af62a90aff3bdef4
zortazert <zortazert@matthewevan.xyz>
2024-03-11 21:58:38 -0500

    fly go brr die go yes

--- js/script.js
+++ js/script.js
@@ -8,36 +8,105 @@ canvas.height = 640;
 const halfWidth = canvas.width / 2;
 const halfHeight = canvas.height / 2;
 
-const gravity = 1;
+const gravity = 0.7;
+var die = false;
+
+var eagle = loadImage('./img/eagle.png');
+var floor = loadImage('./img/floor.png');
+
+function loadImage(src) {
+    var img = new Image();
+    img.src = src;
+    return img;
+}  
+
+class ground {
+    constructor(pos, velocity) {
+        this.pos = pos;
+        this.velocity = velocity;
+    }
+
+    update() {
+        if (die) {
+            return;
+        }
+
+        this.pos.x += this.velocity.x;
+        if (this.pos.x < -floor.width) {
+            this.pos.x += floor.width;
+        }
+    }
+
+    draw() {
+        ctx.drawImage(floor, this.pos.x, this.pos.y);
+        ctx.drawImage(floor, this.pos.x + floor.width, this.pos.y);
+    }
+}
 
 class bird {
     constructor(pos, velocity, radius) {
         this.pos = pos;
         this.velocity = velocity;
         this.radius = radius;
+        this.frameX = 0;
+        this.frameCount = 0;
+        this.frameSpeed = 5;
+        this.numberOfFrames = 4;
     }
 
     up() {
         this.velocity.y = -10;
-        console.log(this.velocity.y);
+        console.log("flap");
     }
+    
     update() {
+        console.log(die);
+        if (die == true) {
+            return;
+        }
         this.velocity.y += gravity;
         this.pos.y += this.velocity.y;
+
+        // Animation from spritesheet code must be more modular coming soon
+        this.frameCount++;
+        // I am controlling how long it displays the frame
+        if (this.frameCount % this.frameSpeed == 0) {
+            this.frameX++;
+            // Go back to first frame
+            if (this.frameX >= this.numberOfFrames) {
+                this.frameX = 0;
+            }
+            this.frameCount=0;
+        }
     };
     
     draw() {
-        ctx.fillStyle = "#ffffff";
-        //ctx.fillRect(this.pos.x, this.pos.y, this.radius, this.radius);
-        ctx.beginPath();
-        ctx.arc(this.pos.x, this.pos.y, this.radius, 0, Math.PI * 2);
-        ctx.fill();
-        ctx.stroke();
+        ctx.save();
+
+        ctx.translate(this.pos.x + this.radius / 2, this.pos.y + this.radius / 2);
+        let rotation = Math.atan2(this.velocity.y, 17);
+
+        // Rotate the canvas by the calculated angle
+        ctx.rotate(rotation);
+
+        // Draw the bird
+        ctx.drawImage(
+            eagle, 
+            this.frameX * 26 * 2, 0,
+            26*2, 18*2,
+            -this.radius / 2, -this.radius / 2, // Draw the bird centered at (0, 0)
+            26*2, 18*2
+        );
+
+        // Restore the previous transformation state of the canvas
+        ctx.restore();
     };
 }
 
 
 const Bird = new bird(vec2(halfWidth - 50, halfHeight), vec2(5,0), 15)
+const Floor = new ground(vec2(0, canvas.height-floor.height), vec2(-5, 5))
+console.log(canvas.height - floor.height);
 
 function startGame() {
     gameLoop();
@@ -60,10 +129,14 @@ function vec2(x, y) {
 
 function gameUpdate() {
     Bird.update();
+    Floor.update();
+
+    floorCollide();
 }
 
 function gameDraw() {
     Bird.draw();
+    Floor.draw();
 }
 
 function gameLoop() {
@@ -73,11 +146,19 @@ function gameLoop() {
     
     gameUpdate();
     gameDraw()
-    
 }
+
+function floorCollide() {
+    if (Bird.pos.y >= (canvas.height - (floor.height + eagle.height + 25))) {
+        die = true;
+        console.log(die)
+    }
+}
+
 document.body.onkeyup = function(e) {
-    if (e.key == " ") {
+    if (die == false && e.key == " ") {
         Bird.up();
     }
 }
+
 gameLoop();
